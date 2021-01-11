9d9e783b1a2bf0be7199b2e935f661212c34cd50
ArunSb28 <sbarun000@gmail.com>
2021-01-11 20:03:55 +0530

    Updated the properties

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('/arun');
+        var socket = new SockJS('/wsEndpoint');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
